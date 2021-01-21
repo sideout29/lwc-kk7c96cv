77bebc4ab56527ce700aa13529b67ea31ae2ab8a
sideout29 <sideout29@gmail.com>
2021-01-21 21:15:27 +0000

    First commit

---
+++ src/index.stories.js
@@ -0,0 +1,6 @@
+import "@lwc/synthetic-shadow";
+import "https://unpkg.com/@salesforce-ux/design-system@2.13.1/assets/styles/salesforce-lightning-design-system.min.css";
+import { createElement } from "lwc";
+import Basic from "./basic";
+
+export const story = () => createElement("c-basic", { is: Basic });
